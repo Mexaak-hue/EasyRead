b1829e022bd53ee3945f5140ac456abf99d307fe
Akbar-K <akbarkamolov24@gmail.com>
2024-06-10 21:13:29 +0400

    Корзина
    счетчик

--- source/script/basket.js
+++ source/script/basket.js
@@ -1,10 +1,11 @@
+
 let substract = document.querySelector('#substract');
 let add = document.querySelector('#add');
 console.log(substract)
 substract.addEventListener("click",function(){
     let output = document.querySelector('#result');
     let result = Number( output.innerHTML )-1;
-    if(result >=0){
+    if(result>=0){
         output.innerHTML=result;
     }
     console.log(output);
